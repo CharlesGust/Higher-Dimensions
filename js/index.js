c88702de20bbfd4224be703dc404bd4b7e843023
Caldwerl <caldwerl@gmail.com>
2014-08-27 10:17:39 -0700

    combine JS into index.js

--- js/index.js
+++ js/index.js
@@ -18,11 +18,6 @@ function User() {
   var list = $(".navigation li");
   var $navLinks = $(".navigation__link");
   var $navTitle = $(".navigation__link--title");
-  var mouseSemaphore = false;
-
-  $(".navigation").world("page", { transition:".5s ease-in-out", rotateY: 0, z:25});
-
-  $navLinks.world("page", { transition: ".45s ease-in-out" });
 
   $navBarLI.on("mouseenter", function(ev) {
     if (!mouseMoved(ev.clientX, ev.clientY)) return;
@@ -42,6 +37,61 @@ function User() {
   });
 
 }
+//End User
+
+
+function ViewControl () {
+
+  var $header = $('.navigation');
+  var $links = $('.navigation .navigation__link');
+  var $aside = $('.sidebar');
+  var $buttons = $('.sidebar div');
+  var currentActive = '#base';
+
+  $('#base').on('click', initializeBase);
+  $('#2-5').on('click', initializeTwoFive);
+  $('#3-D').on('click', initializeThreeD);
+
+  function changeActiveButton (button) {
+
+    $(button)
+      .addClass('sidebar__button--active')
+      .removeClass('sidebar__button');
+    $(currentActive)
+      .addClass('sidebar__button')
+      .removeClass('sidebar__button--active');
+
+    currentActive = button;
+  }
+
+  function initializeBase () {
+
+    $header.world('page', 'clearAll');
+    $links.world('page', 'clearAll');
+    $aside.world('page', 'clearAll');
+    $buttons.world('page', 'clearAll');
+
+    changeActiveButton('#base');
+  }
+
+  function initializeTwoFive () {
+
+    $header.world('page', {transition:".5s ease-in-out", z: -5, rotateX: -5});
+    $links.world('page', {transition:".5s ease-in-out", z: 5, rotateX: 5});
+
+    $aside.world('page', {transition:".5s ease-in-out", z: -5, rotateY: 5});
+    $buttons.world('page', {transition:".5s ease-in-out", z: 5, rotateY: -5});
+
+    changeActiveButton('#2-5');
+  }
+
+  function initializeThreeD () {
+
+
+  }
+}
+//End ViewControl
+
 
 //
 //
@@ -52,5 +102,8 @@ $(document).ready(function() {
   // what is our state coming in? We are going to assume 2D for the time
   // being, but it may be that we have to get this from SessionStorage
   // because we might be in 2D, 2.5D or 3D
+
+  var viewC = new ViewControl ();
+
   var user = new User();
 });
